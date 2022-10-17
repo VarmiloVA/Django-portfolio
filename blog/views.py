61b4c756fa28287d170adee3805d3018cbe12363
VarmiloVA <varmilo.blue@protonmail.com>
2022-10-17 16:47:35 +0200

    Progress made in the frontend

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def index(request):
     """Main Blog page"""
-    return request(request, 'learning_logs/index.html')
+    return render(request, 'blog/index.html')
