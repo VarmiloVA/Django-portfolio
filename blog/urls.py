c3d6b0c80daf87cf6ff1b62b1971fc7c00da921f
VarmiloVA <varmilo.blue@protonmail.com>
2022-11-20 20:58:57 +0100

    progress made on views and urls related code

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+"""URLs patterns for blog app"""
+from django.urls import path
+
+from . import views
+app_name = "blog"
+urlpatterns = [
+    #index
+    path('', views.index, name='index'),
+    #topics
+    path('topic/', views.topic, name='topic'),
+    #about me
+    path('about_me/', views.about_me, name='about_me')
+] 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Topic, Content
 
-# Create your views here.
+def index(request):
+    """Blog main page"""
+    return render(request, 'blog/index.html')
+
+def topic(request):
+    """All topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'#ver para que sirve en el libro'}
+    return render(request, 'blog/topic.html', context)
+
+def about_me(request):
+    """Who's writting this?"""
+    return render(request, 'blog/about_me.html')
