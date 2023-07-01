f6d1c4b9dce6d73010c7f195852f15e3d96d2095
VarmiloVA <varmilo.blue@protonmail.com>
2023-07-01 22:41:12 +0200

    project restored to scratch

--- portfolio/urls.py
+++
@@ -1,9 +0,0 @@
-"""URLs patterns for portfolio app"""
-from django.urls import path
-from . import views
-
-app_name = "portfolio"
-urlpatterns = [
-    #index (the portfolio itself)
-    path('', views.index, name="index")
-]
