03e26f4ca2fa9cc4c690bf4dda760c4ac44b450c
VarmiloVA <varmilo.blue@protonmail.com>
2023-07-03 01:22:07 +0200

    entry model created

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 
 class Topic(models.Model):
-    """A topioc which the user is learning about"""
+    """A topic which the user is learning about"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """Returns a representation of the model as a string"""
         return self.text
+    
+class Entry(models.Model):
+    """What the user has learned about a topic"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Returns a representation of the model as a string"""
+        if len(self.text) > 50:
+            return f"""{self.text[:50]}..."""
+        
+        else:
+            return self.text
