1cc13b09e86749bba3708954d1d888cad1549f85
VarmiloVA <varmilo.blue@protonmail.com>
2023-07-03 01:58:35 +0200

    first templates for blog created

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,6 +53,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'learning_log.urls'
+
+TEMPLATE_DIRS = (
+    os.path.join('learning_logs/', 'templates'),
+)
 
 TEMPLATES = [
     {
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Topic
 
-# Create your views here.
+def index(request):
+    """Index page for learning_logs"""
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Page in which all topics will be shown"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
